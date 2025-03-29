0a1f6cac26fe8ee2a0648ad987cd1e73b4398faf
qkdr <kskwkodh@gmail.com>
2025-03-29 04:05:07 +0300

    jsjs.lua

---
+++ jsjs.lua
@@ -0,0 +1,362 @@
+local Luna = loadstring(game:HttpGet("https://raw.githubusercontent.com/qkdr/workdk.lua/refs/heads/main/qpjf.lua", true))()
+
+-- إنشاء مجلد مفتوح
+Luna:AddFolder({
+    folderName = "سكربتات بلو لوك",
+    folderDescription = "• جميع سكربتات بلوك لوك •",
+    scripts = {
+        {
+            name = "سكربت الاول",
+            description = "شغال ✅",
+            url = "https://nicuse.xyz/MainHub.lua"
+        },
+        {
+            name = "سكربت الثاني",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/OhhMyGehlee/fo/refs/heads/main/ot"
+        }, 
+        {
+        name = "سكربت ثالث",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/Bhusant/Bathroom-attack/refs/heads/main/Bathroom"
+    }, 
+    {
+    name = "سكربت رابع",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/LHking123456/jvJzb8FPs5TuR/refs/heads/main/BLRivals"
+            }, 
+            {
+           name = "سكربت الخامس",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/gumanba/Scripts/refs/heads/main/BlueLock"
+       }
+     }
+})          
+          Luna:AddFolder({
+    folderName = "سكربتات بلوكس فورت",
+    folderDescription = "• جميع سكربتات بلوكس فورت •",
+    scripts = {
+        {
+        name = "سكربت الاصفر",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/AnDepZaiHub/AnDepZaiHubBeta/refs/heads/main/AnDepZaiHubNewUpdated.lua"
+            },
+            {
+            name = "سكربت REDz Hub ⛩️",
+            description = "شغال ✅",
+            url = "https://pastefy.app/ACOX6D6h/raw"
+          }, 
+          {
+          name = "سكربت Tsuo Hub",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/Tsuo7/TsuoHub/main/Tsuoscripts"
+        }, 
+        {
+        name = "سكربت Astral Hub بدون مفتاح",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/Overgustx2/Main/refs/heads/main/BloxFruits_25.html"
+            }, 
+            {
+            name = "سكربت Zinner Hub",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/HoangNguyenk8/Scripts/refs/heads/main/Loader.lua"
+            }, 
+            {
+            name = "سكربت Untie X Hub",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/Akhres/Unit-X/refs/heads/main/Blox%20Fruits.txt"
+          }, 
+          {
+          name = "سكربت Monster Hub",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/giahuy2511-coder/MonsterHub/refs/heads/main/MonsterHub"
+            } 
+          } 
+})
+
+Luna:AddFolder({
+    folderName = "سكربتات ماب سمك",
+    folderDescription = "• جميع سكربتات ماب سمك هنا•",
+    scripts = {
+        {
+        name = "سكربت Speed Hub X",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua"
+            }, 
+            {
+            name = "سكربت SolixHub Fisch",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/debunked69/Solixreworkkeysystem/refs/heads/main/solix%20new%20keyui.lua"
+            }, 
+            {
+            name = "سكربت Raito Hub",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/Efe0626/RaitoHub/refs/heads/main/Script"
+           }, 
+           {
+           name = "سكربت Hidden",
+            description = "تحديث ❌",
+            url = "https://raw.githubusercontent.com/tulontop/HiddenRevamp/refs/heads/main/Loader.luau"
+            }
+          }
+})
+
+Luna:AddFolder({
+    folderName = "سكربتات بروكهافن",
+    folderDescription = "• جميع سكربتات بروكهافن هنا •",
+    scripts = {
+        {
+        name = "سكرب SP Hub",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/as6cd0/SP_Hub/refs/heads/main/Brookhaven"
+            }, 
+            {
+            name = "سكرب MangoHub",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/rogelioajax/lua/main/MangoHub"
+            }, 
+            {
+            name = "سكربت HYZ0R HUB",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/Hyz0r-Dev404/Hyz0/refs/heads/main/Working%25Hyz0r-Hub.md"
+            }, 
+            {
+            name = "سكربت SanderXHub",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/kigredns/SanderXV4.2.2/refs/heads/main/NormalSS.lua"
+            }, 
+            {
+            name = "سكربت عربي",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/ahmidd409/TrollX-V2/refs/heads/main/TrollX40%2540%25409%25409%25409%25409%25409"
+            }
+          } 
+})
+
+Luna:AddFolder({
+    folderName = "سكربتات ام ام تو",
+    folderDescription = "• جميع سكربتات ام ام تو هنا•",
+    scripts = {
+    {
+    name = "سكربت SolixHub",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/debunked69/Solixreworkkeysystem/refs/heads/main/solix%20new%20keyui.lua"
+            }, 
+            {
+            name = "سكربت Vertex",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/vertex-peak/vertex/refs/heads/main/loadstring"
+            }, 
+            {
+            name = "سكربت YARHM",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/Joystickplays/psychic-octo-invention/main/yarhm.lua"
+            }, 
+            {
+            name = "سكربت Auto Farm",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/NoCapital2/MM2Autofarm/main/script"
+            }, 
+            {
+            name = "سكربت R3TH PRIV",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/zxclua/m/main/script"
+            }, 
+            {
+            name = "سكربت Mars Hub",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/1andonlymars/MarsHub/main/MM2"
+            }
+          } 
+}) 
+
+Luna:AddFolder({
+    folderName = "سكربتات War Tycoon",
+    folderDescription = "• جميع سكربتات War Tycoon هنا•",
+    scripts = {
+    {
+    name = "سكربت War Tycoon",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/MariyaFurmanova/Library/main/WarTycoon"
+            }, 
+            {
+            name = "سكربت  War Tycoon",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/JinxTheCatto/Neptune/main/NeptuneHub.lua"
+            }
+          }
+})
+
+Luna:AddFolder({
+    folderName = "سكربتات ماب البحث عن الزر | العرب",
+    folderDescription = "• جميع سكربتات ماب البحث عن الزر | العرب هنا •",
+    scripts = {
+    {
+    name = "سكربت الاول",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/qkdr/sjskjs/refs/heads/main/vsjsk.lua"
+            }
+          } 
+})
+
+Luna:AddFolder({
+    folderName = "سكربتات ماب البحث عن الاناناس | العرب",
+    folderDescription = "• جميع سكربتات ماب البحث عن الاناناس | العرب هنا •",
+    scripts = {
+    {
+    name = "سكربت الاول",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/qkdr/sjskjs/refs/heads/main/kdoej.lua"
+            } 
+         } 
+}) 
+
+Luna:AddFolder({
+    folderName = "سكربتات ماب البحث عن الكود 🌟| العرب",
+    folderDescription = "• جميع سكربتات ماب البحث عن الكود 🌟| العرب هنا •",
+    scripts = {
+    {
+    name = "سكرب الاول",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/qkdr/sjskjs/refs/heads/main/keod.lua"
+            } 
+          }
+})
+
+Luna:AddFolder({
+    folderName = "سكربتات ماب Dead Rails [Alpha]",
+    folderDescription = "• جميع سكربتات ماب Dead Rails [Alpha] هنا •",
+    scripts = {
+    {
+    name = "سكربت NEOX HUB",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/hassanxzayn-lua/NEOXHUBMAIN/refs/heads/main/loader"
+            }, 
+            {
+            name = "سكربت SolixHub",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/debunked69/Solixreworkkeysystem/refs/heads/main/solix%20new%20keyui.lua"
+            }, 
+            {
+            name = "سكربت NEOXHUB",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/hassanxzayn-lua/NEOXHUBMAIN/refs/heads/main/loader"
+            }, 
+            {
+            name = "سكرب SpiderXHub Dead Rails",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/SpiderScriptRB/Dead-Rails-SpiderXHub-Script/refs/heads/main/SpiderXHub%202.0.txt"
+            }, 
+            {
+            name = "سكربت Dead Rails",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/gumanba/Scripts/refs/heads/main/DeadRails"
+            }, 
+            {
+            name = "سكربت SpeedHubX",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua"
+            }, 
+            {
+            name = "سكربت الاخير",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/Emplic/deathrails/refs/heads/main/bond"
+            } 
+          } 
+})
+
+Luna:AddFolder({
+    folderName = "سكربتات ماب بليد بول",
+    folderDescription = "• جميع سكربتات ماب بليد بول هنا•",
+    scripts = {
+    {
+    name = "سكربت Plutonium",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/PawsThePaw/Plutonium.AA/main/Plutonium.Loader.lua"
+            }, 
+            {
+            name = "سكربت Blade Ball 0.1",
+            description = "شغال صح ",
+            url = "https://raw.githubusercontent.com/pid4k/scripts/main/BeanzHub.lua"
+            }, 
+            {
+            name = "سكربت Ronix Hub",
+            description = "شغال ✅",
+            url = "https://api.luarmor.net/files/v3/loaders/79ab2d3174641622d317f9e234797acb.lua"
+            }, 
+            {
+            name = "سكربت Blade Ball",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/r4mpage4/4R4mpageHub/refs/heads/main/FixedAntiCheat.lua"
+            }, 
+            {
+            name = "سكربت Xera",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/luascriptsROBLOX/BladeBallXera/main/XeraUltron"
+            }, 
+            {
+            name = "سكربت Pi-Hub ",
+            description = "شغال ✅",
+            url = "https://pi-hub.pages.dev/protected/loader.lua"
+            }
+          } 
+})
+
+Luna:AddFolder({
+    folderName = "سكربتات ماب PETS GO! ✨ [10x Luck]",
+    folderDescription = "• جميع سكربتات ماب PETS GO! ✨ [10x Luck] هنا •",
+    scripts = {
+    {
+    name = "سكربت Pets GO",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/OhhMyGehlee/rel/refs/heads/main/el"
+            }, 
+            {
+            name = "سكربت LDS Hub",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/SenhorLDS/ProjectLDSHUB/main/OfficialScript"
+            }, 
+            {
+            name = "سكربت Reaper Hub",
+            description = "شغال ✅",
+            url = "https://reaperscripts.com/loader.lua"
+            }, 
+            {
+            name = "سكربت Banana Hub",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/diepedyt/bui/refs/heads/main/BananaHubLoader.lua"
+            }, 
+            {
+            name = "سكربت Zap Hub",
+            description = "شغال ✅",
+            url = "https://www.zaphub.xyz/Exec"
+            }, 
+            {
+            name = "سكربت SpeedHubX",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua"
+            }
+          }
+})
+
+Luna:AddFolder({
+    folderName = "سكربتات ماب DOORS 👁️",
+    folderDescription = "• جميع سكربتات ماب DOORS 👁️ هنا •",
+    scripts = {
+    {
+    name = "سكربت Kodbol Hub",
+            description = "شغال ✅",
+            url = "https://pastefy.app/awczkDwJ/raw"
+            }, 
+            {
+            name = "سكربت Kitty Cats",
+            description = "شغال ✅",
+            url = "https://raw.githubusercontent.com/CatEnddroid/Kitty-Cats-Doors-Beta/refs/heads/main/hub.lua"
+            }, 
+            name = "سكربت Sensation",
+            description = "شغال ✅",
+            url = "https://api.luarmor.net/files/v3/loaders/730854e5b6499ee91deb1080e8e12ae3.lua"
+            }
+          }
+}) 
+Luna:Show()
